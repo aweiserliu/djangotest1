bb747dac0bc0dc9d269b4a74e1052fb22bf5fd3b
aweiser <970271658@qq.com>
2018-04-19 19:35:49 +0800

    web_01

--- djangotest1/urls.py
+++ djangotest1/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.static import serve
 from . import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
